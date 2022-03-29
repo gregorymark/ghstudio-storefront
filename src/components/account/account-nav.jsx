a7a9e3616c7111d4210e97a54c7e3b965714fcb4
Greg Hannan <gregorymarkhannan@gmail.com>
2022-03-29 18:26:50 +0100

    Added log out button to account

--- src/components/account/account-nav.jsx
+++ src/components/account/account-nav.jsx
@@ -1,12 +1,18 @@
 import React from "react"
 import { Link } from "gatsby"
 import { accountNav } from "../../styles/modules/account.module.css"
+import { useCustomer } from "../../hooks/use-customer"
 
 const AccountNav = () => {
+  const {
+    actions: { clearCustomer },
+  } = useCustomer()
+
   return (
     <div className={accountNav}>
       <Link to="/shop/account">Account details</Link>
       <Link to="/shop/account/order-history">Order history</Link>
+      <button onClick={() => clearCustomer()}>Log out</button>
     </div>
   )
 }
--- src/context/customer-context.js
+++ src/context/customer-context.js
@@ -142,6 +142,7 @@ export const CustomerProvider = props => {
           loginCustomer,
           updateCustomerDetails,
           retrieveOrders,
+          clearCustomer,
         },
       }}
     />
