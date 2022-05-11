a0f89054ab62864b5d7db5cda80fda3e099055fa
Greg Hannan <gregorymarkhannan@gmail.com>
2022-05-11 23:39:37 +0100

    Reinstated the grid masonry layout. Hopefully also calling layout onStartLoad will fix layout issues. N.B. onLoad wasn't being called if the image was cached. onStartLoad hopefully is called once the image at least has its layout.

--- src/components/products/product-list-item.jsx
+++ src/components/products/product-list-item.jsx
@@ -1,40 +1,54 @@
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
-import React, { useMemo } from "react"
+import React, { useState, useMemo, useEffect, useRef, useCallback } from "react"
 import { usePrice } from "../../hooks/use-price"
 import { useRegion } from "../../hooks/use-region"
 import ProductLink from "../product-link"
 import {
   productListItem,
   productListItemContent,
-  productImage,
   productInfo,
   productLink,
   productPrice,
 } from "../../styles/modules/product-list.module.css"
 
-const ProductListItem = ({ product }) => {
+const ProductListItem = ({ product, rowHeight = null, screenWidth = null }) => {
   const {
     actions: { getFromPrice },
   } = usePrice()
+
+  const [gridRowEnd, setGridRowEnd] = useState()
+
+  const itemContentEl = useRef()
 
   const { region } = useRegion()
 
   const imageData = getImage(product.thumbnail)
 
+  const updateGridRowEnd = useCallback(() => {
+    if (rowHeight && screenWidth) {
+      const rowSpan = Math.ceil(
+        (itemContentEl.current.getBoundingClientRect().height + 40) / rowHeight
+      )
+      setGridRowEnd("span " + rowSpan)
+    }
+  }, [rowHeight, screenWidth, itemContentEl.current])
+
+  useEffect(() => {
+    updateGridRowEnd()
+  }, [updateGridRowEnd])
+
   const fromPrice = useMemo(() => {
     return getFromPrice(product, region?.currency_code)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [product, region?.currency_code])
+  }, [product, region?.currency_code, getFromPrice])
 
   return (
-    <div className={productListItem}>
+    <div className={productListItem} style={{ gridRowEnd }}>
       <div>
-        <div className={productListItemContent}>
+        <div className={productListItemContent} ref={itemContentEl}>
           <GatsbyImage
             image={imageData}
             alt={product.title}
-            objectFit="contain"
-            className={productImage}
+            onStartLoad={updateGridRowEnd}
           />
           <div className={productInfo}>
             <h3>
@@ -42,7 +56,9 @@ const ProductListItem = ({ product }) => {
                 {product.title}
               </ProductLink>
             </h3>
-            <div className={productPrice}><span>from</span> {fromPrice}</div>
+            <div className={productPrice}>
+              <span>from</span> {fromPrice}
+            </div>
           </div>
         </div>
       </div>
