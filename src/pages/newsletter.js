be672eb102a1079bd7a94e24dbb4df86a0049c4e
Greg Hannan <gregorymarkhannan@gmail.com>
2022-05-15 11:58:38 +0100

    Switched newsletter signup to use sendgrid form for now.

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -1,8 +1,25 @@
 import React from "react"
-import NewsletterSignup from "../components/newsletter-signup"
+import {
+  signupWrap,
+  info,
+  iframe,
+} from "../styles/modules/newsletter.module.css"
 
 const NewsletterPage = () => {
-  return <NewsletterSignup />
+  return (
+    <div className={signupWrap}>
+      <h1>Newsletter</h1>
+      <p className={info}>
+        Enter your name and email address for occasional information from me
+        about my art practice, upcoming shows and news from the shop.
+      </p>
+      <iframe
+        className={iframe}
+        title="Newsletter Signup"
+        src="https://cdn.forms-content.sg-form.com/8f1e605b-d43a-11ec-b3e8-0aa53ceb7d26"
+      />
+    </div>
+  )
 }
 
 export default NewsletterPage
