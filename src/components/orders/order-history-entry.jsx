de1889aa123503ca16b1b9e97f4ce5e51a680371
Greg Hannan <gregorymarkhannan@gmail.com>
2022-05-23 16:54:47 +0100

    Added a query button to order history entries

--- src/components/orders/order-history-entry.jsx
+++ src/components/orders/order-history-entry.jsx
@@ -1,17 +1,28 @@
 import React from "react"
 import OrderBulletin from "./order-bulletin"
-import { orderHistoryEntry } from "../../styles/modules/order-history.module.css"
+import {
+  orderHistoryEntry,
+  orderHeader,
+  orderQueryLink,
+} from "../../styles/modules/order-history.module.css"
 import OrderProductsList from "./order-products-list"
 
 const OrderHistoryEntry = ({ order }) => {
   const date = new Date(order.created_at).toLocaleDateString()
 
+  const contactEmailHref = `mailto:greg@greghannan.studio?Subject=Order%20Query%20#GHSO_${order.display_id}`
+
   return (
     <div className={orderHistoryEntry}>
-      <h2>
-        Order <span className="defFont">#</span>
-        {`GHSO_${order.display_id}`} - {date}
-      </h2>
+      <div className={orderHeader}>
+        <h2>
+          Order <span className="defFont">#</span>
+          {`GHSO_${order.display_id}`} - {date}
+        </h2>
+        <a href={contactEmailHref} className={orderQueryLink}>
+          Have a query?
+        </a>
+      </div>
       <OrderBulletin order={order} />
       <h3>Order items</h3>
       <OrderProductsList
