385c74f6ac9918c3d0373fc08522a27434668343
Greg Hannan <gregorymarkhannan@gmail.com>
2022-05-26 16:06:28 +0100

    Removed unused imports

--- src/components/header/shop-nav.jsx
+++ src/components/header/shop-nav.jsx
@@ -1,9 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 import { useCustomer } from "../../hooks/use-customer"
-import { shopNav, cartIcon, regionSelect } from "../../styles/modules/header.module.css"
+import { shopNav, cartIcon } from "../../styles/modules/header.module.css"
 import CartIcon from "../cart/cart-icon"
-import RegionSelect from "./region-select"
 
 const ShopNav = ({ setNavOpen, isCheckout }) => {
   const { loading, customer } = useCustomer()
